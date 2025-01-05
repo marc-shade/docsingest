e4d8a1a64eedb388aa2d635ba76ff58553bb81a7
Marc Shade <marc@2acrestudios.com>
2025-01-05 18:09:45 -0500

    Bump version to 0.1.28

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('README.md', 'r', encoding='utf-8') as f:
 
 setup(
     name='docsingest',
-    version = "0.1.27",
+    version = "0.1.28",
     description='📄 AI-Powered Document Analysis Tool for Comprehensive Document Processing',
     long_description=long_description,
     long_description_content_type='text/markdown',
